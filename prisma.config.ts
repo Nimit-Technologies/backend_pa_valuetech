404cd10c7590058627628e395eee2e2e592a6c06
Its-Pranjal <pranjalcodes.ai@gmail.com>
2026-09-16 15:54:35 +0530

    prisma.config

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_URL,
+    url:
+      process.env.NODE_ENV === "production"
+        ? process.env.DIRECT_URL
+        : process.env.DATABASE_URL,
   },
 });
